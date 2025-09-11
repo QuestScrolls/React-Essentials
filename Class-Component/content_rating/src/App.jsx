07e46fc861613fc8641f004c86aff396d0c92ee0
QuestScrolls <mixiyomi@gamil.com>
2025-09-11 05:55:04 -0400

    Class Component Like Dislike

---
+++ Class-Component/content_rating/src/App.jsx
@@ -0,0 +1,14 @@
+import ContentRating from './Components/ContentRating'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <ContentRating/>
+    </>
+  )
+}
+
+export default App
