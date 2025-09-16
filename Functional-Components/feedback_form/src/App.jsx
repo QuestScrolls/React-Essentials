14a42607791fddc77d45773f1b951ad942a6f0f1
QuestScrolls <mixiyomi@gamil.com>
2025-09-16 05:27:21 -0400

    feedBack

---
+++ Functional-Components/feedback_form/src/App.jsx
@@ -0,0 +1,14 @@
+import FeedBackForm  from './Components/FeedbackForm'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <FeedBackForm/>
+    </>
+  )
+}
+
+export default App
